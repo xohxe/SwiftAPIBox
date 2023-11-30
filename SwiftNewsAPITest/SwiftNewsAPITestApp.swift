a9aa84b68ebaeb82e478ab103a305ee7f571d414
xohxe <saeyan2477@gmail.com>
2023-11-30 14:30:16 +0900

    Initial Commit

---
+++ SwiftNewsAPITest/SwiftNewsAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsAPITestApp.swift
+//  SwiftNewsAPITest
+//
+//  Created by 김소혜 on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
